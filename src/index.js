b4480042f09049ccfeda250dbcfba32372eb5a5f
jaydeep-popat <popatjaydeep21@gmail.com>
2025-01-21 21:48:48 +0530

    utis are defined

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import dotenv from 'dotenv';
 import connectDB from "./db/index.js";
-
+import app from './app.js'
 
 dotenv.config({
     path: './env'
 })
-connectDB();
+
+connectDB()
+.then(()=>{
+
+    app.app2.get('/',(req,res)=>{
+        res.send("hi"); 
+    })
+
+   app.app2.listen(process.env.PORT,()=>{
+    console.log("surver is running on port", process.env.PORT);  
+})
+})
+.catch((err)=>{
+    console.log("mongo not connected", err)
+
+
+})
---
+++ src/utils/apiResponse.js
@@ -0,0 +1,8 @@
+class apiResponse{
+    constructor(status, data, message="Success"){
+        this.statusCode=statusCode
+        this.data = data;
+        this.message = message;
+        this.success= this.statusCode< 400
+    }
+}
